826242def681b6434c06d460b59df9b395314646
nabigh9091 <152377309+nabigh9091@users.noreply.github.com>
2023-12-13 15:15:29 +0500

    added api info for swagger

--- src/main/java/com/seulah/appdesign/config/OpenApiConfig.java
+++ src/main/java/com/seulah/appdesign/config/OpenApiConfig.java
@@ -1,25 +1,39 @@
 package com.seulah.appdesign.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+import java.util.function.Predicate;
 
 @Configuration
 @EnableWebMvc
 public class OpenApiConfig {
     @Bean
-    public OpenAPI openApiInformation() {
-        Server localServer = new Server()
-                .url("http://localhost:8090");
-        Info info = new Info()
-                .title("Seulah CMS Api Documentation")
-                .version("V1.0.0")
-                .license(new License());
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .paths(Predicate.not(PathSelectors.regex("/error.*")))
+                .build().apiInfo(apiInfo());
+    }
 
-        return new OpenAPI().info(info).addServersItem(localServer);
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Seualh REST API",
+                "CMS Api Documentation.",
+                "V1",
+                "",
+                new Contact("", "", ""),
+                "", "", Collections.emptyList());
     }
 }
