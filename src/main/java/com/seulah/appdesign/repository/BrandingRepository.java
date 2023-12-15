ed6618d2249e594ec25e9558a9c74942beccdedd
nabigh9091 <152377309+nabigh9091@users.noreply.github.com>
2023-12-15 11:37:07 +0500

    handle duplication exception

--- src/main/java/com/seulah/appdesign/repository/BrandingRepository.java
+++ src/main/java/com/seulah/appdesign/repository/BrandingRepository.java
@@ -4,4 +4,5 @@ import com.seulah.appdesign.entity.Branding;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BrandingRepository extends MongoRepository<Branding, String> {
+    Branding findByBrandName(String brandingName);
 }
