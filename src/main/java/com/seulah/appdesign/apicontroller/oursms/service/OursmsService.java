7f3e92b82f6331f997b4ebfd2869b9c495309d8b
Arham <152394241+arhamTns@users.noreply.github.com>
2024-02-13 13:06:22 +0500

    update

--- src/main/java/com/seulah/appdesign/apicontroller/oursms/service/OursmsService.java
+++ src/main/java/com/seulah/appdesign/apicontroller/oursms/service/OursmsService.java
@@ -3,6 +3,7 @@ package com.seulah.appdesign.apicontroller.oursms.service;
 import com.seulah.appdesign.apicontroller.oursms.dto.SmsRequest;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -18,22 +19,32 @@ public class OursmsService {
         this.restTemplate = restTemplate;
     }
 
-    public String ourSms(String mobileNumber,String otp){
-        smsRequest = new SmsRequest();
-        smsRequest.setSrc("SeulahFin");;
-        smsRequest.setDests(new String[]{mobileNumber, mobileNumber});
-        smsRequest.setBody(otp);
+    public String ourSms(String mobileNumber, String otp) {
+        try {
+            if (mobileNumber != null) {
+                smsRequest = new SmsRequest();
+                smsRequest.setSrc("SeulahFin");
+                ;
+                smsRequest.setDests(new String[]{mobileNumber, mobileNumber});
+                smsRequest.setBody(otp);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(bearerToken);
-        headers.setContentType(MediaType.APPLICATION_JSON);
+                HttpHeaders headers = new HttpHeaders();
+                headers.setBearerAuth(bearerToken);
+                headers.setContentType(MediaType.APPLICATION_JSON);
 
-        HttpEntity<SmsRequest> requestEntity = new HttpEntity<>(smsRequest,headers);
-      ResponseEntity<Object> response = restTemplate.postForEntity(apiUrl,
-               requestEntity, Object.class);
-        if(response.getBody()!=null){
-            return "true";
+                HttpEntity<SmsRequest> requestEntity = new HttpEntity<>(smsRequest, headers);
+                ResponseEntity<Object> response = restTemplate.postForEntity(apiUrl,
+                        requestEntity, Object.class);
+                if (response.getBody() != null) {
+                    return "true";
+                }
+                return "false";
+            } else {
+                return "Mobile number not be empty.";
+            }
+        } catch (HttpClientErrorException.BadRequest e) {
+            return e.getMessage();
         }
-        return "false";
+
     }
 }
