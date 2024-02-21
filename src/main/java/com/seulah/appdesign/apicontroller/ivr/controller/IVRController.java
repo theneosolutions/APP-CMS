da2aaa5b1c85e0fa05b4da66df14d9d596ce9443
Arham <152394241+arhamTns@users.noreply.github.com>
2024-02-21 15:40:29 +0500

    @RestController added

--- src/main/java/com/seulah/appdesign/apicontroller/ivr/controller/IVRController.java
+++ src/main/java/com/seulah/appdesign/apicontroller/ivr/controller/IVRController.java
@@ -1,11 +1,9 @@
 package com.seulah.appdesign.apicontroller.ivr.controller;
 
 import com.seulah.appdesign.apicontroller.ivr.service.IVRService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @RequestMapping("/api/v1/cms")
 @CrossOrigin(origins = {"http://localhost:3000","http://localhost:3001","http://localhost:8085"}, maxAge = 3600, allowCredentials = "true")
 public class IVRController {
